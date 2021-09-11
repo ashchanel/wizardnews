b9786e7b583e36f3d8a13fba2fda16e95a700910
ashchanel <ash_bussell@yahoo.com>
2021-09-11 11:47:42 -0500

    final commit

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+        <a href="/posts/${post.id}">${post.title}
+        <p>
+        <span class="news-position">${post.id}. ▲</span>${post.title}
+        <small>(by ${post.name})</small>
+      </p>
+        </a>         
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+  res.send(html);
+});
+
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (!post.id) {
+    throw new Error("Something not found");
+  }
+  res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.content} upvotes | ${post.date}
+          </small>
+        </div>`);
+});
+
+const PORT = 1337;
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(404).send("Something broke!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
